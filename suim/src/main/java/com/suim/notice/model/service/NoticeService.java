60e187725057ca2d58b861bff94de77c936531ee
LEEJH9713 <jihwa1tp@daum.net>
2023-05-31 15:26:03 +0900

    mybatis-config 에 타입 및 매퍼 지정 및 공지사항 조회, 상세조회 기능 구현

--- suim/src/main/java/com/suim/notice/model/service/NoticeService.java
+++ suim/src/main/java/com/suim/notice/model/service/NoticeService.java
@@ -5,11 +5,24 @@ import java.util.ArrayList;
 import com.suim.common.model.vo.PageInfo;
 import com.suim.notice.model.vo.Notice;
 
+/**
+ * ArrayList<Notice> selectList(PageInfo pi);
+ * -> 공지사항 리스트 조회
+ * 
+ * Notice selectBoard(int noticeNo);
+ * -> 공지사항 상세 조회
+ * @author jihwan
+ *
+ */
 public interface NoticeService {
 
 	int selectListCount();
 
 	ArrayList<Notice> selectList(PageInfo pi);
+
+	int increaseCount(int noticeNo);
+	
+	Notice selectBoard(int noticeNo);
 
 	
 	
