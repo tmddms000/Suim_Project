0698c063c225f4d9677cf3a6fa3ba17b6206e65e
leetaehwa <128684882+e-taehwa@users.noreply.github.com>
2023-05-31 20:45:26 +0900

    자유게시판 (#19)
    리뷰 : 리스트조회, 등록
    베스트 글 : 리스트조회,상세페이지
    리스트 : 댓글 수 표시
    
    Co-authored-by: 97cmj <115520750+97cmj@users.noreply.github.com>

--- suim/src/main/java/com/suim/board/controller/BoardController.java
+++ suim/src/main/java/com/suim/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -15,18 +16,18 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.suim.board.model.service.BoardService;
 import com.suim.board.model.vo.Battachment;
 import com.suim.board.model.vo.Board;
+import com.suim.board.model.vo.Reply;
 import com.suim.common.model.vo.PageInfo;
 import com.suim.common.template.Pagination;
 
@@ -44,7 +45,7 @@ public class BoardController {
 			@RequestParam(value="cPage", defaultValue="1") int currentPage,
 			ModelAndView mv) {
 		
-		// 페이징처리를 위한 PageInfo 객체 얻어내기
+
 		int listCount = boardService.selectListCount();
 		
 		int pageLimit = 10;
@@ -54,15 +55,7 @@ public class BoardController {
 		
 		ArrayList<Board> list = boardService.selectList(pi);
 		ArrayList<Board> blist = boardService.selectbList();
-		
 
-		
-		
-		
-		
-		// mv.addObject 메소드는 반환형이 ModelAndView 객체를 다시 리턴하는 형태이기 때문에
-		// 다음과 같이 메소드 체이닝을 이용해서 한번에 응답데이터와 응답뷰를 지정할 수 있다.
-		
 		mv.addObject("pi", pi)
 		  .addObject("list", list)
 		  .addObject("blist", blist)
@@ -74,108 +67,25 @@ public class BoardController {
 	@RequestMapping("enrollForm.bo")
 	public String enrollForm() {
 		
-		// /WEB-INF/views/board/boardEnrollForm.jsp
 		return "board/free-boardEnrollForm";
 	}
-	
-	
-	   @PostMapping("/board/create")
-	    public String createBoard(@ModelAttribute Board board,
-	                              @RequestParam("attachmentFile") MultipartFile attachmentFile) {
 
-	        // 게시물 정보 저장 처리
-	        board.setBoardDate(new Date(0));
-	        board.setStatus("Y");
-	        // ... (나머지 필드 설정)
-	        // 게시물 저장 로직 실행
-
-	        // 첨부 파일 처리
-	        if (!attachmentFile.isEmpty()) {
-	            String originalFilename = attachmentFile.getOriginalFilename();
-	            String fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
-	            String savedFileName = UUID.randomUUID().toString() + fileExtension;
-
-	            File saveFile = new File("/resources/uploadFiles" + savedFileName);
-	            try {
-	                attachmentFile.transferTo(saveFile);
-
-	                // 첨부 파일 정보 저장 처리
-	                Battachment attachment = new Battachment();
-	                attachment.setOriginName(originalFilename);
-	                attachment.setChangeName(savedFileName);
-	                attachment.setCreateDate(new Date(0));
-	                attachment.setFileStatus("Y");
-	                attachment.setBoardNo(board.getBoardNo()); // 게시물 번호 설정
-	                // ... (나머지 필드 설정)
-	                // 첨부 파일 저장 로직 실행
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	                // 파일 저장 실패 시 예외 처리
-	            }
-	        }
-
-	        // 게시물 작성 완료 후 페이지 이동 등 필요한 처리
-
-	        return "redirect:/" + board.getBoardNo(); // 예시로 게시물 상세 페이지로 이동하는 URL을 반환합니다.
-	    }
-	    
-	
-	@RequestMapping(value="/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
-	@ResponseBody
-	public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request )  {
-		JsonObject jsonObject = new JsonObject();
-		
-        /*
-		 * String fileRoot = "C:\\summernote_image\\"; // 외부경로로 저장을 희망할때.
-		 */
-		
-		// 내부경로로 저장
-		String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
-		String fileRoot = contextRoot+"resources/uploadFiles/";
-		
-		String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
-		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
-		String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
-		
-		File targetFile = new File(fileRoot + savedFileName);	
-		try {
-			InputStream fileStream = multipartFile.getInputStream();
-			FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
-			jsonObject.addProperty("url", "resources/uploadFiles/"+savedFileName); // contextroot + resources + 저장할 내부 폴더명
-			jsonObject.addProperty("responseCode", "success");
-				
-		} catch (IOException e) {
-			FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
-			jsonObject.addProperty("responseCode", "error");
-			e.printStackTrace();
-		}
-		String a = jsonObject.toString();
-		return a;
-	}
-	
-	// 쿼리스트링 방식
 		@RequestMapping("detail.bo")
 		public ModelAndView selectBoard(ModelAndView mv,
 										int bno) {
-			// bno 에는 상세조회하고자 하는 해당 게시글의 번호가 담겨있음
-			// System.out.println("bno : " + bno);
-			
-			// 1. 해당 게시글 조회수 증가용 서비스 호출 및 결과 받기 (update 하고 옴)
+
 			int result = boardService.increaseCount(bno);
 			
-			
-			// 2. 만약 게시글 조회수가 성공적으로 증가되었다면 그제서야 select 요청
+
+
 			if(result > 0) { // 성공
-				
-				// boardDetiailView.jsp 에서 필요로 하는 응답데이터를 조회
+
 				Board b = boardService.selectBoard(bno);
 				
-				// 조회된 데이터를 mv 에 담아서 포워딩 페이지 경로를 잡아주기
 				mv.addObject("b", b).setViewName("board/free-boardDetail");
 				
 			} else { // 실패
-				
-				// 에러문구를 담아서 에러페이지로 포워딩 경로 잡아주기
+
 				mv.addObject("errorMsg", "게시글 상세조회 실패").setViewName("common/errorPage");
 			}
 			
@@ -188,7 +98,6 @@ public class BoardController {
 								  String filePath,
 								  HttpSession session) {
 			
-			// System.out.println("bno : " + bno);
 			
 			int result = boardService.deleteBoard(bno);
 			
@@ -202,7 +111,7 @@ public class BoardController {
 					new File(realPath).delete();
 				}
 				
-				// 일회성 알람 문구 담고 게시판 리스트페이지로 url 재요청
+
 				session.setAttribute("alertMsg", "성공적으로 게시글이 삭제되었습니다.");
 				
 				return "redirect:/list.bo";
@@ -214,9 +123,135 @@ public class BoardController {
 				return "common/errorPage";
 			}
 		}
-	
-	
-	
-	
+		
+		@ResponseBody
+		@RequestMapping(value = "rlist.bo", produces = "application/json; charset=UTF-8")
+		public String ajaxSelectReplyList(int bno) {
+			
+			ArrayList<Reply> list = boardService.selectReplyList(bno);
+			
+			return new Gson().toJson(list);
+		}
+		
+		@ResponseBody
+		@RequestMapping(value = "rinsert.bo", produces = "text/html; charset=UTF-8")
+		public String ajaxInsertReply(Reply r) {
+			
 
+			
+			int result = boardService.insertReply(r);
+			
+			return (result > 0) ? "success" : "fail";
+		}
+		
+		@RequestMapping(value="/uploadSummernoteImageFile", produces = "application/json; charset=utf8")
+		@ResponseBody
+		public String uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request) {
+		    JsonObject jsonObject = new JsonObject();
+
+		    // 내부경로로 저장
+		    String contextRoot = new HttpServletRequestWrapper(request).getRealPath("/");
+		    String fileRoot = contextRoot + "resources/fileupload/";
+
+		    String originalFileName = multipartFile.getOriginalFilename();    // 오리지날 파일명
+		    String extension = originalFileName.substring(originalFileName.lastIndexOf("."));    // 파일 확장자
+		    String savedFileName = UUID.randomUUID() + extension;    // 저장될 파일 명
+		    
+		    File targetFile = new File(fileRoot + savedFileName);
+		    try {
+		        InputStream fileStream = multipartFile.getInputStream();
+		        FileUtils.copyInputStreamToFile(fileStream, targetFile);    // 파일 저장
+		        jsonObject.addProperty("url", "/resources/fileupload/" + savedFileName); // contextroot + resources + 저장할 내부 폴더명
+		        jsonObject.addProperty("responseCode", "success");
+		        
+		    } catch (IOException e) {
+		        FileUtils.deleteQuietly(targetFile);    // 저장된 파일 삭제
+		        jsonObject.addProperty("responseCode", "error");
+		        e.printStackTrace();
+		    }
+		    String a = jsonObject.toString();
+		    return a;
+		}
+		
+		@RequestMapping("insert.bo")
+		public String insertBoard(Board b,
+								 Battachment ba,
+								MultipartFile upfile,
+								HttpSession session,
+								Model model) {
+			
+			if(!upfile.getOriginalFilename().equals("")) { // 넘어온 첨부파일이 있을 경우
+				
+				String changeName = saveFile(upfile, session);
+				
+				
+				ba.setOriginName(upfile.getOriginalFilename());
+				ba.setChangeName("resources/uploadFiles/" + changeName);
+			}
+			
+			// 이 시점 기준으로
+			// 넘어온 첨부파일이 있다면
+			// boardTitle, boardWriter, boardContent, originName, changeName
+			// 필드에 값들이 담겨 있음
+			// 넘어온 첨부파일이 없다면
+			// boardTitle, boardWriter, boardContent
+			// 필드에 값들이 담겨 있음
+			int result = boardService.insertBoard(b,ba);
+			
+
+			if(result > 0) { // 성공 => 일회성 알람문구 띄운 뒤 게시글 리스트페이지로 url 재요청
+				
+				session.setAttribute("alertMsg", "성공적으로 게시글이 등록되었습니다.");
+				
+				return "redirect:/list.bo"; // 내부적으로 1번 페이지로 향함
+				
+			} else { // 실패 => 에러 문구를 담아서 에러페이지로 포워딩
+				
+				model.addAttribute("errorMsg", "게시글 등록 실패");
+				
+				return "common/errorPage";
+			}
+		}
+		public String saveFile(MultipartFile upfile, HttpSession session) {
+			
+			// 파일명 수정 작업 후 서버에 업로드 시키기
+			// => 왠만해선 파일명이 겹치지 않게끔 !!
+			
+			// MyFileRenamePolicy 에서 지정했던 로직 그대로 재현
+			// 예) "bono.jpg" => "20230511104425xxxxx.jpg"
+			// 1. 원본파일명 뽑기
+			String originName = upfile.getOriginalFilename(); // "bono.jpg"
+			
+			// 2. 현재 시간 형식을 문자열로 뽑아내기
+			String currentTime = new SimpleDateFormat("yyyyMMddHHmmss")
+									 .format(new Date(0)); // "20230511104920"
+			
+			// 3. 뒤에 붙을 5자리 랜덤값 뽑기 (10000 ~ 99999 범위)
+			int ranNum = (int)(Math.random() * 90000 + 10000); // 13152
+			
+			// 4. 원본파일명으로부터 확장자명 뽑기
+			String ext = originName.substring(originName.lastIndexOf(".")); // ".jpg"
+			
+			// 5. 2, 3, 4 단계에서 구한 값을 모두 이어 붙이기
+			String changeName = currentTime + ranNum + ext;
+			
+			// 6. 업로드 하고자 하는 서버의 물리적인 경로 알아내기
+			String savePath = session.getServletContext().getRealPath("/resources/uploadFiles/");
+			
+			// 7. 경로와 수정파일명을 합체 후 파일을 업로드 해주기
+			// MultipartFile 객체에서 제공하는 transferTo 메소드
+			// [ 표현법 ]
+			// upfile.transferTo(업로드하고자하는파일객체);
+			try {
+				upfile.transferTo(new File(savePath + changeName));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return changeName;
+		}
+
+	
+	
+		   
 }
--- suim/src/main/java/com/suim/board/model/service/BoardService.java
+++ suim/src/main/java/com/suim/board/model/service/BoardService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.suim.board.model.vo.Battachment;
 import com.suim.board.model.vo.Board;
+import com.suim.board.model.vo.Reply;
 import com.suim.common.model.vo.PageInfo;
 
 public interface BoardService {
@@ -21,6 +22,9 @@ public interface BoardService {
 	// 게시글 등록
 	int insertBoard(Board b, Battachment ba);
 	
+	// 게시글 등록
+	int insertBattachment(Battachment ba);
+	
 	// 게시글 상세조회 서비스
 	
 	// 게시글 조회수 증가
@@ -29,6 +33,17 @@ public interface BoardService {
 	Board selectBoard(int boardNo);
 	// 게시글 삭제 서비스
 	int deleteBoard(int boardNo);
+	
+	// 댓글 리스트 조회 서비스 (Ajax)
+	ArrayList<Reply> selectReplyList(int boardNo);
+	
+	// 댓글 작성 서비스 (Ajax)
+	int insertReply(Reply r);
+	
+	// 댓글 수량 
+	int ReplyCount(int boardNo);
+	
+
 	
 
 }
